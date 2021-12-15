35b43d7f99b25a4acb820c710024087e999f0f86
Nate Li <colson.com@gmail.com>
2021-12-15 23:30:17 +1100

    add task and task-item component add db.json add task service add angular font awesome need to know how to use 'npm run server'

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,40 @@
+import {Task} from "./Task";
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2.30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Nurse Appointment',
+    day: 'May 6th at 2.30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Detain Appointment',
+    day: 'May 8th at 8.30pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2.30pm',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Doctors Appointment',
+    day: 'May 23th at 10.30pm',
+    reminder: true,
+  },
+  {
+    id: 6,
+    text: 'Doctors Appointment',
+    day: 'May 27th at 9.30am',
+    reminder: false,
+  },
+]
